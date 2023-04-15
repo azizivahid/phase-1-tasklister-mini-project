05c9c66a831c79b9ff4b3bb6bd8060ab9e109ebc
azizivahid <azizi.vahid@yahoo.com>
2023-04-15 16:16:26 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const inputField = document.getElementById("new-task-description");
+  const formElement = document.querySelector("form");
+  formElement.addEventListener('submit', (event) => {
+  let newtask =inputField.value
+
+  event.preventDefault();
+    if (newtask.length===0){
+    alert('Task cannot be empty')
+    }else{
+      handleToDo(newtask);
+    }
+    formElement.reset()
+  });
 });
+
+function handleToDo (newtask) {
+  let listItem = document.createElement("li");
+  let btn = document.createElement('button')
+
+  btn.addEventListener('click',handleDelete)
+  btn.textContent='X'
+  listItem.innerText = `${newtask}`;
+  listItem.appendChild(btn)
+  document.querySelector("#tasks").appendChild(listItem);
+}
+
+function handleDelete(e){
+e.target.parentNode.remove()
+}
